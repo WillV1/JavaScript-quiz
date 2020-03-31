d748dff1c6c29768c28c590504ba83a5ec4559e4
WillV1 <wmvanhook@gmail.com>
2020-03-31 06:58:32 -0400

    Updating JS to create carousel effect

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
 
-const startQuiz = document.getElementById("start");
-const questionOne = document.getElementById("next-question");
+
+const question = document.getElementById("next-question");
 const countdown = document.querySelector(".time");
 const timeUp = document.getElementById("time-up");
 
-let question = ['Common data types DO NOT include:']
+let questions = ['Common data types DO NOT include:']
+
+function carousel(){
+    let box = document.querySelector('.carouselbox');
+    let start = document.getElementById("start");
+    let counter = 0;
+    let amount = questions.length;
+    let current = questions[0];
+    box.classList.add('active');
+    function navigate(direction) {
+        current.classList.remove('current');
+        counter = counter + direction;
+        if (direction === -1 && 
+            counter < 0) {
+                counter = amount - 1;
+            }
+        if (direction === 1 &&
+            !questions[counter]) {
+            counter = 0
+            }
+        current = questions[counter];
+        current.classList.add('current');        
+    }
+    next.addEventListener('click', function(ev) {
+        navigate(1);
+    });
+    navigate(0);
+    }
 
 
 function myFunction(){
-    alert(question)
-}
+    question.style.display = ['Common data types DO NOT include:']
+    console.log(question);
+} 
 
-startQuiz.onclick = myFunction;
+question.onclick = myFunction;
+
+question.addEventListener('click', myFunction);
 //let questionTwo = {
 //    'Arrays in JavaScript can be used to store:': {
 //        1: 'Numbers and strings',
