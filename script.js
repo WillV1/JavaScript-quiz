3619561f6f715d5b1511e95ec9f3dc18a4891c6a
WillV1 <wmvanhook@gmail.com>
2020-04-01 17:40:59 -0400

    Updated script and html

--- script.js
+++ script.js
@@ -1,66 +1,89 @@
 let start = document.getElementById("start");
 let questions = document.querySelector('.questions');
-let questionOne = document.getElementById("question-one");
+//let questionOne = document.getElementById("question-one");
 let answerFirstOne = document.getElementById("answer1-one");
 let answerFirstTwo = document.getElementById("answer1-two");
 let answerFirstThree = document.getElementById("answer1-three");
 let answerFirstFour = document.getElementById("answer1-four");
 
-let questions2 = document.querySelector('.questions');
-let questionTwo = document.getElementById("question-two");
+let questionsSectionTwo = document.querySelector('.questions-section-two');
+//let questionTwo = document.getElementById("question-two");
 let answerSecondOne = document.getElementById("answer2-one");
 let answerSecondTwo = document.getElementById("answer2-two");
 let answerSecondThree = document.getElementById("answer2-three");
 let answerSecondFour = document.getElementById("answer2-four");
 
-let questions3 = document.querySelector('.questions');
+
 let questionThree = document.getElementById("question-three");
 let answerThreeOne = document.getElementById("answer3-one");
 let answerThreeTwo = document.getElementById("answer3-two");
 let answerThreeThree = document.getElementById("answer3-three");
 let answerThreeFour = document.getElementById("answer3-four");
 
-
-
-
 function quizOne () {
-questions.style.display = "block";
-questionOne.textContent =  'Commonly used data types DO NOT include:'
-answerFirstOne.textContent = 'A: Strings'
-answerFirstTwo.textContent = 'B: Booleans'
-answerFirstThree.textContent = 'C: Alerts' 
-answerFirstFour.textContent = 'D: Numbers'
+let quizFirst = document.createElement('p');
+quizFirst.id = 'question-one';     
+quizFirst.innerHTML = 'Commonly used data types DO NOT include:';    
+document.querySelector('.questions').insertBefore(quizFirst, answerFirstOne);   
 
-alert(questionOne);
-console.log(questionOne);  
+const questionOne = {
+    'A:': 'Strings',
+    'B:': 'Booleans',
+    'C:': 'Alerts',
+    'D:': 'Numbers'
+}    
+const quizQuestionOne = Object.entries(questionOne);
+console.log(quizQuestionOne);
+questions.style.display = "block";
+
+for (i = 0; i < quizQuestionOne.length; i++){
+    answerFirstOne.textContent = quizQuestionOne[0];
+    answerFirstTwo.textContent = quizQuestionOne[1];
+    answerFirstThree.textContent = quizQuestionOne[2];
+    answerFirstFour.textContent = quizQuestionOne[3];
+}
+alert(quizQuestionOne); 
 }
 start.addEventListener('click', quizOne);
 
 function quizTwo () {
-    questions2.style.display = "block";
-    questionOne.textContent =  'Arrays in JavaScript can be used to store:'
-    answerFirstOne.textContent = 'A: Other Arrays'
-    answerFirstTwo.textContent = 'B: Numbers and Strings'
-    answerFirstThree.textContent = 'C: Booleans' 
-    answerFirstFour.textContent = 'D: All of the Above' 
-}
-    answerFirstOne.addEventListener('click', quizTwo);
-    answerFirstTwo.addEventListener('click', quizTwo);
-    answerFirstThree.addEventListener('click', quizTwo);
-    answerFirstFour.addEventListener('click', quizTwo);
-
-function quizThree () {
-    questions3.style.display = "block";
-    questionOne.textContent =  'A very useful tool during development and debugging for printing content to the debugger is:'
-    answerFirstOne.textContent = 'A: JavaScript'
-    answerFirstTwo.textContent = 'B: Terminal/Bash'
-    answerFirstThree.textContent = 'C: For Loops' 
-    answerFirstFour.textContent = 'D: Console.log' 
+    let quizSecond = document.createElement('p');
+    quizSecond.id = 'question-two';     
+    quizSecond.innerHTML = 'Arrays in JavaScript can be used to store:';    
+    document.querySelector('questions-section-two').insertBefore(quizSecond, answerSecondOne);   
+    
+    const questionTwo = {
+        'A:': 'Other Arrays',
+        'B:': 'Numbers and Strings',
+        'C:': 'Booleans',
+        'D:': 'All of the Above'
+    }    
+    const quizQuestionTwo = Object.entries(questionTwo);
+    questionsSectionTwo.style.display = "block";
+    
+    for (i = 0; i < quizQuestionTwo.length; i++){
+        answerSecondOne.textContent = quizQuestionTwo[0];
+        answerSecondTwo.textContent = quizQuestionTwo[1];
+        answerSecondThree.textContent = quizQuestionTwo[2];
+        answerSecondFour.textContent = quizQuestionTwo[3];
     }
-    answerSecondOne.addEventListener('click', quizThree);
-    answerSecondTwo.addEventListener('click', quizThree);
-    answerSecondThree.addEventListener('click', quizThree);
-    answerSecondFour.addEventListener('click', quizThree);   
+    start.addEventListener('click', quizTwo);
+    start.addEventListener('click', quizTwo);
+    start.addEventListener('click', quizTwo);
+    start.addEventListener('click', quizTwo);
+}
+//function quizThree () {
+    
+//    questionOne.textContent =  'A very useful tool during development and debugging for printing content to the debugger is:'
+//    answerFirstOne.textContent = 'A: JavaScript'
+//    answerFirstTwo.textContent = 'B: Terminal/Bash'
+//    answerFirstThree.textContent = 'C: For Loops' 
+//    answerFirstFour.textContent = 'D: Console.log' 
+//    }
+//    answerSecondOne.addEventListener('click', quizThree);
+//    answerSecondTwo.addEventListener('click', quizThree);
+//    answerSecondThree.addEventListener('click', quizThree);
+//    answerSecondFour.addEventListener('click', quizThree);   
 
 const countdown = document.querySelector(".time");
 const timeUp = document.getElementById("time-up");
@@ -87,6 +110,6 @@ countdownToGame();
   function endGame() {
   countdown.textContent = " ";
   const endCounter = timeUp.textContent = 'GAME OVER!';
-  document.getElementsByName('time-up').appendChild(endCounter);
+  document.getElementsById('time-up').appendChild(endCounter);
   }
   countdownToGame();
